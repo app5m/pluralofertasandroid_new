3c0d64f8c2ff6a584bda4791215e838e992ed5ca
joao <joaocapitaneo@gmail.com>
2021-07-15 00:43:45 -0300

    consegui fazer a dialogFragment abrir na activity

--- app/src/main/java/com/example/pluralofertasandroid2/activity/offerDetails/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/pluralofertasandroid2/activity/offerDetails/ProductDetailsActivity.kt
@@ -1,12 +1,8 @@
 package com.example.pluralofertasandroid2.activity.offerDetails
 
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +12,6 @@ import com.example.pluralofertasandroid2.fragments.dialog.DialogCuponsFrag
 import com.example.pluralofertasandroid2.fragments.productDetailsBottom.TabsDetailsBottomFragment
 import com.example.pluralofertasandroid2.helper.MyUsefulKotlin
 import com.example.pluralofertasandroid2.helper.RecyclerItemClickListener
-import com.google.android.material.internal.ContextUtils.getActivity
 import kotlinx.android.synthetic.main.activity_details_product.*
 import kotlinx.android.synthetic.main.tool_bar.*
 
@@ -37,18 +32,14 @@ class ProductDetailsActivity : AppCompatActivity(), RecyclerItemClickListener{
         toolbar.visibility = View.VISIBLE
 
         MyUsefulKotlin().startFragment(TabsDetailsBottomFragment(), supportFragmentManager)
+        miguito.setOnClickListener() {
 
+            DialogCuponsFrag().show(supportFragmentManager, "DialogCuponsFrag")
+        }
         btnComprarDetalhesProduto.setOnClickListener {
             val intent = Intent(this, CartCheckoutActivity::class.java)
-
-            this.miguito.setOnClickListener() {
-               /* Log.d(TAG, "onClick: opening dialog")
-                val dialog = DialogCuponsFrag()
-                dialog.setTargetFragment(TabsDetailsBottomFragment(), 1)
-                supportFragmentManager.let { it1 -> dialog.show(it1,"DialogCuponsFrag") }*/
-            }
-
             startActivity(intent)
+
         }
     }
 
