f21657f7591343258aaa9f23c5eec67a51d0d079
Vilvock <64989193+Vilvock@users.noreply.github.com>
2021-09-17 13:32:53 -0300

    safer

--- app/src/main/java/br/com/app5m/pluralofertas/fragments/home/mainMenu/UpdatePasswordFragment.kt
+++ app/src/main/java/br/com/app5m/pluralofertas/fragments/home/mainMenu/UpdatePasswordFragment.kt
@@ -2,33 +2,32 @@ package br.com.app5m.pluralofertas.fragments.home.mainMenu
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.Fragment
 import br.com.app5m.pluralofertas.R
 import br.com.app5m.pluralofertas.activity.MainActivity
-import br.com.app5m.pluralofertas.controller.UAddressControl
 import br.com.app5m.pluralofertas.controller.UserControl
 import br.com.app5m.pluralofertas.controller.webservice.WSResult
-import br.com.app5m.pluralofertas.fragments.dialog.RegisterAddressDialog
 import br.com.app5m.pluralofertas.helper.MyUsefulKotlin
 import br.com.app5m.pluralofertas.helper.Preferences
-import br.com.app5m.pluralofertas.helper.RecyclerItemClickListener
 import br.com.app5m.pluralofertas.helper.Validation
-import br.com.app5m.pluralofertas.model.UAddress
 import br.com.app5m.pluralofertas.model.User
-import kotlinx.android.synthetic.main.fragment_edit_profile.*
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_siginup.*
+import kotlinx.android.synthetic.main.fragment_update_password.*
 
-class ProfileEditFragment: Fragment(), WSResult {
 
+/**
+ * A simple [Fragment] subclass.
+ */
+class UpdatePasswordFragment : Fragment(), WSResult {
 
     private lateinit var useful: MyUsefulKotlin
     private lateinit var userControl: UserControl
-    private lateinit var uAddressControl: UAddressControl
     private lateinit var preferences: Preferences
     private lateinit var validation: Validation
 
@@ -41,7 +40,7 @@ class ProfileEditFragment: Fragment(), WSResult {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? { //start view
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
 
     }
 
@@ -56,22 +55,32 @@ class ProfileEditFragment: Fragment(), WSResult {
         builder = AlertDialog.Builder(requireContext())
         alertDialog = builder.create()
 
-        userControl.listId()
-
         loadClicks()
 
     }
 
     override fun uResponse(list: List<User>, type: String) {
 
+        useful.closeLoading(alertDialog)
+
         val user = list[0]
 
-        nomeCompletoMeuPerfilEditText.setText(user.name)
-        emailMeuPerfilEditText.setText(user.email)
-        cpfMeuPerfilEditText3.setText(user.cpf)
+        if (user.status.equals("01")) {
+
+            Toast.makeText(requireContext(), "Senha alterada com sucesso!", Toast.LENGTH_LONG)
+                .show()
+
+            newPassword_et.setText("")
+            coPassword_et.setText("")
+
+        } else {
+
+            Toast.makeText(requireContext(), user.msg, Toast.LENGTH_SHORT).show()
+
+        }
+
 
     }
-
 
     override fun error(error: String) {
         useful.closeLoading(alertDialog)
@@ -80,43 +89,20 @@ class ProfileEditFragment: Fragment(), WSResult {
 
     private fun loadClicks() {
 
-        btnVoltarMeuPerfilTextView.setOnClickListener {
-            requireActivity().onBackPressed()
-        }
+        updatePass_bt.setOnClickListener {
 
-        btnEditarPerfil.setOnClickListener {
+            if (!validate()) return@setOnClickListener
+
+            userControl.updatePassword(newPassword_et.text.toString())
 
 
-/*        {
-            "id": 3,
-            "nome": "Cadastro teste",
-            "email": "cadastro@cadastro.com",
-            "cpf": "02737594006",
-            "celular":"(51)9888888",
-            "data_nascimento": "12/08/1991",
-            "token": "plural_ofertas@2021"
-        }*/
-
-            val updateUser = User()
-
-            userControl.updateUserData(updateUser)
-
-        }
-
-        btnAlterarSenhaMeuPerfilTextView.setOnClickListener {
-            useful.startFragmentOnBack(UpdatePasswordFragment(), requireActivity().supportFragmentManager)
         }
 
     }
-/*
-    private fun validate(): Boolean {
-        return if (!validation.name(fullname_edittext)) false
-        else if (!validation.email(email_edittext)) false
-        else if (!validation.cpf(cpf_edittext)) false
-        else if (!validation.cellphone(phone_edittext)) false
-        else if (!validation.password(login_password_edittext, 0)) false
-        else validation.coPassword(login_password_edittext, login_password2_edittext)
-    }*/
 
+    private fun validate(): Boolean {
+        return if (!validation.password(newPassword_et, 0)) false
+        else (validation.coPassword(newPassword_et, coPassword_et))
+    }
 
 }
