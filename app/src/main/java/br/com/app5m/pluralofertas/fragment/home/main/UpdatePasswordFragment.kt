f7adb44af7483c414d08207b1d74cdf3b76f9784
Vilvock <64989193+Vilvock@users.noreply.github.com>
2021-12-06 23:12:42 -0300

    sereni por hj

--- app/src/main/java/br/com/app5m/pluralofertas/fragment/home/main/UpdatePasswordFragment.kt
+++ app/src/main/java/br/com/app5m/pluralofertas/fragment/home/main/UpdatePasswordFragment.kt
@@ -33,7 +33,7 @@ class UpdatePasswordFragment : Fragment(), WSResult {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? { //start view
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_update_password, container, false)
 
     }
 
