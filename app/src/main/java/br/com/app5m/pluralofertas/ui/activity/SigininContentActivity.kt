26f5f26aec6e6df700ec22e84f4adddf57d5ce11
Vilvock <64989193+Vilvock@users.noreply.github.com>
2021-12-09 07:32:20 -0300

    bgl n acaba

--- app/src/main/java/br/com/app5m/pluralofertas/ui/activity/SigininContentActivity.kt
+++ app/src/main/java/br/com/app5m/pluralofertas/ui/activity/SigininContentActivity.kt
@@ -1,6 +1,7 @@
 package br.com.app5m.pluralofertas.ui.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import br.com.app5m.pluralofertas.R
 import br.com.app5m.pluralofertas.ui.fragment.login.SignUpContentFragment
@@ -19,12 +20,22 @@ class SigininContentActivity:AppCompatActivity() {
         useful = Useful(this)
 
         useful.startFragment(SignUpContentFragment(), supportFragmentManager)
-        useful.setActionBar(this, supportActionBar!!, "Faça login ou realize seu cadastro", 1)
+        useful.setActionBar(this, supportActionBar!!, "Faça login ou realize seu cadastro", 0)
 
 
     }
 
 
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 
 
